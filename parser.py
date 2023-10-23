dcdc6de1bf71efa08192a7a091f22071a55aaea7
nalexiou <nk.alexiou@gmail.com>
2023-10-23 13:13:49 +0300

    added OS info, changed delimiter to ;

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 # Creates a CSV file with
 # asset (IP), port/protocol, status (open/closed/filtered), application, information
 def createcsv(json_dict,filename):
-  
+
     # Split non-whitespace groups until whitespaces are found. 
     # Matches for example 21/tcp   open    ftp  vsftpd X.X.X
     pattern = r"^(\S+)\s+(\S+)\s+(\S+)\s+(.*)$"
@@ -12,11 +12,8 @@ def createcsv(json_dict,filename):
     with open(filename, "w+") as outf:
 
         for key in json_dict.keys():
-            
             value_list = json_dict[key]
-
             for item in value_list:
-
                 match = re.match(pattern,item)
                 
                 group1 = match.group(1)
@@ -24,34 +21,60 @@ def createcsv(json_dict,filename):
                 group3 = match.group(3)
                 group4 = match.group(4)
 
-                outf.write(key+", "+group1 +", "+group2+ ", "+group3 + ", "+ group4)
+                outf.write(key+"; "+group1 +"; "+group2+ "; "+group3 + "; "+ group4)
                 outf.write("\n")
+
+# create separate csv files with OS information
+def create_os_sv(os_dict,filename):
+
+    with open(filename, "w+") as outf:
+        for key,value in os_dict.items():
+            outf.write(key.strip()+"; "+value)
+            outf.write("\n")
 
 
 # Reads xml zenmap output file and creates a dictionary with Key -> asset/IP and Value -> list of open ports
 def read_file(filename):
-
+    
     with open(filename,'r') as f:
 
         outputfile = "results_"+filename+".txt"
+
+        with open(outputfile, "w") as outf:
         
-        dict_scan_results = defaultdict(list)
-        target = ""
-        line = f.readline()
-        while line:
-            match = re.match(r'^(Nmap scan report for)(.*)', line)
-            if match:
-                target = match.group(2)
+            dict_scan_results = defaultdict(list)
+            dict_os = {}
 
-            else:
-                match2 = re.match(r'^(\d{1,8}\/)', line)
-                if match2:
-                    dict_scan_results[target].append(line)
-            
-            line=f.readline()
+            target = ""
+            line = f.readline()
+            while line:
+                match = re.match(r'^(Nmap scan report for)(.*)', line)
+                if match:
+                    target = match.group(2)
 
-        return dict_scan_results
+                else:
+                    match2 = re.match(r'^(\d{1,8}\/)', line)
+                    if match2:
+                        dict_scan_results[target].append(line)
+                    
+                    else:
+                        #OS details: Microsoft Windows Server 202X build XXXXX - XXXXX
+                        # If OS details line is found
+                        if "OS details" in line:
+                            match3 = re.match(r'^(OS details:)(.*)$',line)
+                            
+                            if match3:
+                                dict_os[target]=match3.group(2).strip()
+                        
+                        # if not OS exact match line is found
+                        elif "No exact OS matches for host" in line:
+                            dict_os[target]="No exact OS matches for host"
+                    
+                line=f.readline()
+
+            return dict_scan_results, dict_os
 
 if __name__ == "__main__":
-    json_dict1 = read_file("the_zenmap_xml_output.xml")
-    createcsv(json_dict1, "csv_filename_to_create.csv")
+    json_dict1, os_dict1 = read_file("nmap-results.xml")
+    createcsv(json_dict1, "nmap-results.csv")
+    create_os_sv(os_dict1, "nmap-results-os.csv")
